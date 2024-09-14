287c23438edcc5910e149a7083fa1421b1ab295d
Bapumafiyas <154887259+Bapumafiyas@users.noreply.github.com>
2024-09-14 15:24:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,44 +10,44 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7498264348:AAH64Ub8e2KKKq5tzw94eirIyUj5Oqg9vRU")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7210056748:AAFbRKFkl3XOrDIjCzZS7xqdzBHnEzV0OWw")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "27158380"))
+APP_ID = int(os.environ.get("APP_ID", "20170562"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "522a2ab8895f062dfd5f2fe7e941f77e")
+API_HASH = os.environ.get("API_HASH", "56aa61b4d1198329f24c1602eb3f73d4")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002037021883"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002052438967"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "6791098303"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7066319676"))
 
 #Port
 PORT = os.environ.get("PORT", "1112")
 
 #Database 
-DB_URI = "mongodb+srv://Ayush:Hijda@zen.haecqvp.mongodb.net/?retryWrites=true&w=majority&appName=Zen"
+DB_URI = "mongodb+srv://Backbenchersnevakakms:<PLs5YvLbUYWRJXk8>@cluster0.u3mmn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "Zipshort.net")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "be510fb8cbf0eeee0e1b9bc14fe591938819ef28")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "tyrano-shortener.in")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "573cf3412c46648705cfe935b79e734abaf4a206")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID","https://t.me/unfiltered_backup")
+TUT_VID = os.environ.get("TUT_VID","https://t.me/BackbenchersBackupOnly/3")
 
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001559911506"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
-    ADMINS=[]
-    for x in (os.environ.get("ADMINS", "").split()):
+    ADMINS=[7066319676]
+    for x in (os.environ.get("ADMINS", "7066319676").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
@@ -56,7 +56,7 @@ except ValueError:
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "This video/Photo/anything is available on the internet. We @LeakHubd or its subsidiary channel doesn't produce any of them.")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "This video/Photo/anything is available on the internet. We @BackbenchersBackupOnly or its subsidiary channel doesn't produce any of them.")
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
@@ -68,7 +68,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1392566136)
+ADMINS.append(7066319676)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
